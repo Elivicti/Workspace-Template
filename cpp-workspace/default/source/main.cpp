fed976d5323614910d9384e5d15c4c0d787eb6d3
Elivicti <edwynb72@gmail.com>
2025-06-07 13:45:49 +0800

    auto set code page to utf-8 on windows

--- cpp-workspace/default/source/main.cpp
+++ cpp-workspace/default/source/main.cpp
@@ -1,5 +1,27 @@
 #include "header.hpp"
 
+#ifdef _WIN32
+#include <Windows.h>
+static struct CodePageSetter
+{
+	UINT OG_InputCP;
+	UINT OG_OutputCP;
+
+	CodePageSetter()
+		: OG_InputCP{ GetConsoleCP() }
+		, OG_OutputCP{ GetConsoleOutputCP() }
+	{
+		SetConsoleCP(CP_UTF8);
+		SetConsoleOutputCP(CP_UTF8);
+	}
+	~CodePageSetter()
+	{
+		SetConsoleCP(OG_InputCP);
+		SetConsoleOutputCP(OG_OutputCP);
+	}
+} win32_console_codepage_setter{};
+#endif
+
 int main(int argc, char* argv[])
 {
 	print("Hello, World!\n");
