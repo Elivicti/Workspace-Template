b6c803898e1542119316839f05210c811bd90b48
Elivicti <edwynb72@gmail.com>
2025-06-22 12:13:08 +0800

    change code page setter class to be defined via macro

--- cpp-workspace/default/include/header.hpp
+++ cpp-workspace/default/include/header.hpp
@@ -14,3 +14,20 @@ void print(std::format_string<Args...> fmt, Args&&... args)
 {
 	print(std::cout, fmt, std::forward<Args>(args)...);
 }
+
+#ifdef _WIN32
+#include <Windows.h>
+#define WIN32_ENSURE_CODEPAGE(cp)    \
+static struct Win32_CodePageSetter { \
+	UINT OG_InputCP;  \
+	UINT OG_OutputCP; \
+	Win32_CodePageSetter()  \
+		: OG_InputCP{ GetConsoleCP() }        \
+		, OG_OutputCP{ GetConsoleOutputCP() } \
+	{ SetConsoleCP(cp); SetConsoleOutputCP(cp); } \
+	~Win32_CodePageSetter() { SetConsoleCP(OG_InputCP); SetConsoleOutputCP(OG_OutputCP); } \
+} win32_console_codepage_setter{}
+#else
+#define WIN32_ENSURE_CODEPAGE(cp) // only useful on windows
+#endif
+
